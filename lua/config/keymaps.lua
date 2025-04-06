608a655a7333b26258c442989ad799f4d6101827
Steffen Bewersdorff <steffen@bleech.de>
2025-04-06 23:50:59 +0200

    remap keymap for floating terminal to <c-Space>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- toggle floating terminal in the cwd
+-- (requires removing the treesitter keymap for incremental selection)
+vim.keymap.set({ "n", "v", "i", "t" }, "<c-Space>", function() Snacks.terminal() end, { desc = "Terminal (cwd)" })
